1ab1a33ade6eacff25b5a869670877db25bb724b
joshwashywash <joshwashywash@gmail.com>
2020-05-16 22:10:05 -0500

    a stray console.log

--- src/client/ConnectionManager.ts
+++ src/client/ConnectionManager.ts
@@ -14,7 +14,6 @@ export default class ConnectionManager {
 		this.socket.on('players', (players: Player[]) => {
 			this.table.players.clear();
 			players.forEach(player => this.table.players.add(player));
-			console.log(this.table.players);
 		});
 
 		// this.table.me?.on('drop', (position: PIXI.IPoint) => {
